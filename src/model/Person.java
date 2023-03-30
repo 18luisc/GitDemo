f59f675df1c4743f87fdb75106015597a74c558d
Luis Lucero <lucero.luis@correounivalle.edu.co>
2023-03-30 16:29:17 -0500

    Enrollment

--- src/model/Person.java
+++ src/model/Person.java
@@ -29,7 +29,9 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + '}';
     }
+
 }
